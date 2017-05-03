fdb2aaba7f4f77b479a025a3cdedde9d7fffda06
Dhiraj <bharatkumar.jogdand@forgeahead.io>
2017-05-03 18:11:08 +0530

    my first commit

---
+++ src/package1/Practice1.java
@@ -0,0 +1,10 @@
+package package1;
+
+/**
+ * Created by Sameer on 5/3/2017.
+ */
+public class Practice1 {
+    public static void main(String[] args) {
+        System.out.println("hello this is first pkg");
+    }
+}
---
+++ src/package2/Practice2.java
@@ -0,0 +1,10 @@
+package package2;
+
+/**
+ * Created by Sameer on 5/3/2017.
+ */
+public class Practice2 {
+    public static void main(String[] args) {
+    System.out.println("hello this is second pkg");
+}
+}
